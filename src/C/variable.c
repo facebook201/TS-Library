3846cd067dbfaa7bb6ebaeee7aca653c0d766a17
syo <syolmm@qq.com>
2020-10-27 22:41:16 +0800

    切片

---
+++ src/C/variable.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+
+int main() {
+  int arr[3] = {1, 2, 3};
+  
+  for (int i = 0; i < 3; i++) {
+    printf("数组地址：%p\n", &arr[i]);
+  }
+
+  printf("整型int的字节大小：%d\n", sizeof(arr[0]));
+  return 0;
+}

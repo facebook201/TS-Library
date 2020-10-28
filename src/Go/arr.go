4cef5255367d1b42ac335b41ae631a864326e89e
syo <syolmm@qq.com>
2020-10-28 18:08:08 +0800

    常用代码合集

---
+++ src/Go/arr.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"fmt"
+);
+
+func main() {
+	// myArr := [...]string{"world", "python", "go"};
+
+	// for _, item := range myArr {
+	// 	fmt.Printf("hello %s\n", item);
+	// }
+	var age = 20;
+	if (age > 10) {
+		fmt.Println("已经成年");
+	}
+}
+
+
+
+
--- src/Go/variable.go
+++ src/Go/variable.go
@@ -5,27 +5,9 @@ import (
 	"unsafe"
 );
 
-
-// 数据类型
-// 1、布尔型 			bool
-// 2、数字类型		int、float32、float64
-// 3、字符串			string	
-// 4、派生类型
-			// * 指针（Pointer）
-			// * 数组类型
-			// * 结构化类型（struct）
-			// * Channel 类型
-			// * 函数类型
-			// * 切片类型
-			// * 接口类型（interface）
-			// * Map类型
-
-
-// Go 的变量
-
 func main() {
 	// 一行声明 一个变量
-	//  Go 中双引号和单引号是不一样的，这里要一定要使用双引号，表示字符串，而在单引号表示rune 类型的字符
+	//  Go 中双引号和单引号是不一样的，这里要一定要使用双引号，表示字符串，而在单引号表示 rune 类型的字符
 	// var name string = "zhangsan";
 	// fmt.Println(name);
 

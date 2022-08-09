e7bc617fa3a17cff52b06d3c68d0b2d5efee775a
syo <syolmm@qq.com>
2022-08-09 08:15:26 +0800

    history 源码

--- src/index.tsx
+++ src/index.tsx
@@ -1,68 +1,76 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
-import { Observable } from 'rxjs';
-
-const stream$ = new Observable(subscriber => {
-  setTimeout(() => {
-    subscriber.next([1, 2, 3]);
-  }, 500);
-
-  setTimeout(() => {
-    subscriber.next({ a: 1000 });
-  }, 1000);
-
-  setTimeout(() => {
-    subscriber.next('end');
-  }, 3000);
-
-  setTimeout(() => {
-    subscriber.complete();
-  }, 4000);
-});
-
-// 启动流
-const subscription = stream$.subscribe({
-  complete: () => console.log('done'),
-  next: v => console.log(v),
-  error: () => console.log('error')
-});
+// const ThemeContext = React.createContext("light");
 
 
-const ThemeContext = React.createContext("light");
+// export default function App() {
+//   const [theme, setTheme] = React.useState({ theme: 'light' });
+
+//   const toggleTheme = () => {
+//     const themeVal = theme.theme === 'light' ? 'dark' : 'light';
+//     setTheme({ theme: themeVal });
+//   };
+
+//   const notify = () => toast('Here is your toast.');
+
+//   return (
+//     <div>
+//       <h1 onClick={toggleTheme}>
+//         I ❤️ U
+//       </h1>
+//       <ThemeContext.Provider value={theme.theme}>
+//         <AppBody />
+//       </ThemeContext.Provider>
+
+//       <section>
+//         <h1 onClick={notify}> New Notify</h1>
+//         <Toaster />
+//       </section>
+//     </div>
+//   );
+// }
+
+// function AppBody() {
+//   const theme = React.useContext(ThemeContext);
+
+//   return (
+//     <div>
+//       {theme}
+//     </div>
+//   );
+// }
 
 
-export default function App() {
-  const [theme, setTheme] = React.useState({ theme: 'light' });
-
-  const toggleTheme = () => {
-    const themeVal = theme.theme === 'light' ? 'dark' : 'light';
-    setTheme({ theme: themeVal });
-  };
-
+function App() {
   return (
     <div>
-      <h1 onClick={toggleTheme}>
-        I ❤️ U
-      </h1>
-      <ThemeContext.Provider value={theme.theme}>
-        <AppBody />
-      </ThemeContext.Provider>
+      App
+      <nav>
+        <Link to="/invoices">Invoices</Link>
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
 
-function AppBody() {
-  const theme = React.useContext(ThemeContext);
+function Invoices() {
+  return <div>Invoices</div>
+}
 
-  return (
-    <div>
-      {theme}
-    </div>
-  );
+function Expenses() {
+  return <div>Expenses</div>
 }
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    {/* 侧边栏导航 */}
+    <Route path="/" component={App} />
+    <Switch>
+      <Route path="/invoices" component={Invoices} />
+      <Route path="/expenses" component={Invoices} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('app')
 );
